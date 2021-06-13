d3b86efacdfec601f347e03cf347952d18217576
sampadadhole <sampadadhole4@gmail.com>
2021-06-13 19:54:46 +0530

    second commit

--- script.js
+++ script.js
@@ -2,57 +2,35 @@ const button = document.querySelector(".get-jokes");
 
 button.addEventListener("click", getJokes);
 
-function getJokes(e){
-    
-    const numb = Math.floor(Math.random()*15);
-    // console.log(numb);
-    const xhr = new XMLHttpRequest();
+function getJokes(e) {
+  const numb = Math.floor(Math.random() * 15);
+  // console.log(numb);
+  const xhr = new XMLHttpRequest();
 
-    xhr.open("GET",`http://api.icndb.com/jokes/random/${numb}`,true);
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${numb}`, true);
 
-    xhr.onload = function(){
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+      let output = "";
 
-    if(this.status === 200){
-        
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        let output ="";
-
-        if(response.type==="success"){
-            // console.log(1);
-            response.value.forEach(function(joke){
-                output +=
-                `<div class="results>
+      if (response.type === "success") {
+        // console.log(1);
+        response.value.forEach(function (joke) {
+          output += `<div class="results>
                 // <div class="emoji"></div>
                 <li><div class="emoji">😂</div>${joke.joke}</li>
                 </div>`;
-                // console.log(output);
-
-                // const test = document.querySelector(".jokes");
-                // test.addEventListener("mouseover", function(event) {
-                //     // console.log("over");
-                //     output +=`<div class="emoji">🤣</div>`;
-                // });
-                
-
-            });
-
-        }
-        else{
-            output +=`something went wrong`;
-        }
-        document.querySelector(".jokes").innerHTML = output;
-        // const footer = document.createElement("footer");
-        // footer += `Made by Love`;
-        // footer.classList.add("footer");
-        
-        
-    
+        });
+      } else {
+        output += `something went wrong`;
+      }
+      document.querySelector(".jokes").innerHTML = output;
     }
-}
-        
-    xhr.send();
+  };
 
-    // document.querySelector("input").value="";
-    e.preventDefault();
+  xhr.send();
+
+  e.preventDefault();
 }
